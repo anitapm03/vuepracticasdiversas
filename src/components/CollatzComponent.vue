a9cdbc9ad7a5190971b966e71de99fa54094fb10
anitapm03 <apereiramolinero@gmail.com>
2023-11-02 09:36:40 +0100

    Práctica collatz

---
+++ src/components/CollatzComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h1>Collatz</h1>
+
+        <label>Inserte un número:</label>
+        <input type="nummber" v-model="numero"/>
+        <button @click="calcularCollatz()">Calcular!</button>
+        <br/>
+
+        <div v-if="numeros.length > 0">
+            <ul>
+                <li v-for="num in numeros" :key="num" v-html="$filters.evaluarNumero(num)"></li>
+            </ul>
+        </div>
+        
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CollatzComponent",
+        methods: {
+            calcularCollatz(){
+                var num = this.numero;
+                var aux=[];
+                while (num != 1){
+                    if (num % 2 == 0){
+                        num = num / 2;
+
+                    } else {
+                        num = num * 3 + 1;
+                    }
+                    aux.push(num);
+                }
+                this.numeros = aux;
+            }
+        },
+        data() {
+            return {
+                numero: -1,
+                numeros: []
+            }
+        }
+    }
+</script>
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <img src="../assets/images/home.webp" style="width: 500px; height: 400px" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeComponent"
+    }
+</script>
